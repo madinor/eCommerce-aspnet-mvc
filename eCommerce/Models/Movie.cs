28212816b9ec94d8d5eae0e632f51a1e367b8432
Noreddine Madi <adam.smit2020@gmail.com>
2023-06-18 00:35:14 -0400

    seed the Database

--- eCommerce/Models/Movie.cs
+++ eCommerce/Models/Movie.cs
@@ -11,7 +11,7 @@ namespace eCommerce.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
-        public string ImageUrl { get; set; }
+        public string ImageURL { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public MovieCategory MovieCategory  { get; set; }
--- eCommerce/Program.cs
+++ eCommerce/Program.cs
@@ -29,4 +29,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+//Seed database
+EcomDbInitializer.Seed(app);
+
+
 app.Run();
