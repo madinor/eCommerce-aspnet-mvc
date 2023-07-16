5ab185cfba3d7d9201844701a9b17b3239ab99ac
Noreddine Madi <adam.smit2020@gmail.com>
2023-07-16 16:34:58 -0400

    Signing up & Signin out users

--- eCommerce/Controllers/AccountController.cs
+++ eCommerce/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using eCommerce.Data;
+using eCommerce.Data.Static;
 using eCommerce.Data.ViewModels;
 using eCommerce.Models;
 using Microsoft.AspNetCore.Identity;
@@ -45,6 +46,42 @@ namespace eCommerce.Controllers
             TempData["Error"] = "Wrong credentials. Please, try again!";
             return View(loginVM);
         }
+
+
         public IActionResult Register() => View(new RegisterVM());
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return View("RegisterCompleted");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Movies");
+        }
+
     }
 }
