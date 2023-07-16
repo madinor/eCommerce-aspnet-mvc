9917a264df15a22b341f779dff27951856faad5a
Noreddine Madi <adam.smit2020@gmail.com>
2023-07-16 13:33:24 -0400

    Adding the AccountController & Designing Login view

---
+++ eCommerce/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using eCommerce.Data;
+using eCommerce.Data.ViewModels;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly EcomDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, EcomDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Login() => View(new LoginVM());
+    }
+}
